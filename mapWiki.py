d6bf7a51659f72679d0261bb7e195f414e02317e
Reed Brace <rbrace@trinity.edu>
2017-07-01 15:21:49 -0700

    got id from RC stream

--- mapWiki.py
+++ mapWiki.py
@@ -1,7 +1,7 @@
 import xml.etree.ElementTree as ET
 import urllib2
 import re
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 import time
 """
 iterate = 0
@@ -11,96 +11,46 @@ def GetRevisions(pageTitle):
     ###altURL = "https://en.wikipedia.org/w/api.php?action=query&titles=September_11_attacks&prop=revisions&rvprop=ids|comment|content&rvlimit=5&rvdifftotext=prev&format=xml&callback=?"
     
     revisions = []                                        #list of all accumulated
-
     next = ''                                             #information for the next request
     while True:
         response = urllib2.urlopen(url + next).read()     #web request
         temp = re.findall('<rev [^>]*>', response)
             
         revisions += temp #adds all revisions from the current request to the list
-
         cont = re.search('<continue rvcontinue="([^"]+)"', response)
         if not cont:                                      #break the loop if 'continue' element missing
             break
-
         next = "&rvcontinue=" + cont.group(1)             #gets the revision Id from which to start the next request
-
     return revisions;
-
 """
 def mostRecent():
-	filt = []	
-	rcURL = "https://en.wikipedia.org/w/api.php?action=query&list=recentchanges&rcprop=title|ids|sizes|flags|user|comment&rclimit=500&format=xml"
-	response = urllib2.urlopen(rcURL).read()
-	tree = ET.parse(response)
-	root = tree.getroot()	
-	#print(root.text)
-	for child in root.findall('edit'):
-		filt += child
-	#zzz = re.findall('<rc type="edit"*>', response)	
-	return filt;
+    filt = []
+    rcURL = "https://en.wikipedia.org/w/api.php?action=query&list=recentchanges&rcprop=title|ids|sizes|user|comment&rclimit=500&format=xml"
+    #rcURL = "https://en.wikipedia.org/w/api.php?action=query&list=recentchanges&rcprop=title|ids|flags&rclimit=500&format=xml"
+    response = urllib2.urlopen(rcURL).read()
+    root = ET.fromstring(str(response))
+    #root = tree.getroot()
+    #print(root.
+    #print(root[1][0])
+    for child in root[1][0]:
+        if 'vandalism' in str(child.attrib):
+            #print(child.attrib)
+            print(str(str(child.attrib).split("'revid': \'")[1].split("\'")[0]))
+            #print(child.attrib)
+        #filt += child
+    #zzz = re.findall('<rc type="edit"*>', response)    
+    return filt;
+
+def locate(IP_List):
+    #'http://api.hostip.info/get_html.php?ip=12.215.42.19&position=true'
+    for IP in IP_List:
+        response = urllib.urlopen('http://api.hostip.info/get_html.php?ip=' + IP + '&position=true').read()
+
 
 test = []
 test = mostRecent()
 print("test")
+for z in test:
+    print(z)
 print(len(test))
 
-
-"""
-revisions = GetRevisions("David_P._Steiner")
-vandalismRev = []
-vandalIDs = []
-revIDs = {}
-print(len(revisions))
-
-for i in revisions:
-    if "vandalism" in i:
-        vandalismRev.append(i)
-        #print(i)
-vandalNum = len(vandalismRev)
-print(vandalNum)
-
-for z in vandalismRev:
-    yoof = z.split("revid=\"")[1].split("\"")
-    #foo = z.split("parentid=\"")[1]
-    #bar = foo.split("\"")[0]
-    revisionID = yoof[0]
-    parentID = yoof[2]
-    revIDs[revisionID] = parentID
-    vandalIDs.append(parentID)
-    #print(bar)
-
-
-def getDiff(pTitle, revList):
-	iterate = 0
-	changes = []
-	header = {'User-Agent': 'Mozilla/5.0'} 
-	wiki = "https://en.wikipedia.org/w/index.php?title="+pTitle+"&diff="
-	if iterate%10==0:
-		time.sleep(5)
-	for d in revList:
-		tempWiki = wiki + d
-		req = urllib2.Request(tempWiki, headers=header)
-		tempPage = urllib2.urlopen(req)
-		tempSoup = BeautifulSoup(tempPage)
-		additions = str(tempSoup.find_all("td", {"class" : "diff-addedline"}))
-		if len(additions.split(" ")) < 1000:
-			changes += "<tr>"
-			changes += additions
-			changes += "</tr>"
-		iterate+=1
-
-	return changes
-
-t = getDiff("David_P._Steiner", vandalIDs)
-#for z in t:
-#	print(z)
-
-filename = "kinkaid.html"
-fz = open(filename, "w")
-fz.writelines(t)
-fz.write("</html>")
-#for z in t:
-#	fz.write(z)
-fz.close()
-"""
